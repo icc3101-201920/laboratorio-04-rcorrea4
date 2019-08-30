c7ef931f9fc90c2935feca1987a72c177555a15f
rcorrea4 <rcorrea4@miuandes.cl>
2019-08-30 18:58:35 -0400

    primer commit

---
+++ Laboratorio_3_OOP_201902/Enums/EnumType.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Laboratorio_3_OOP_201902.Enums
+{
+    class EnumType
+    {
+        public enum EnumTypes
+        {
+            None,
+            melee,
+            range,
+            longRange,
+            buff,
+            buffmelee,
+            buffrange,
+            bufflonngRange,
+            captain,
+            weather
+        }
+
+    }
+}
---
+++ Laboratorio_3_OOP_201902/Game.cs
@@ -0,0 +1,96 @@
+﻿using Laboratorio_3_OOP_201902.Cards;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Laboratorio_3_OOP_201902
+{
+    public class Game
+    {
+        //Atributos
+        private Player[] players;
+        private Player activePlayer;
+        private List<Deck> decks;
+        private Board boardGame;
+        private bool endGame;
+
+        //Constructor
+        public Game()
+        {
+
+        }
+
+        //Propiedades
+        public Player[] Players
+        {
+            get
+            {
+                return this.players;
+            }
+        }
+        public Player ActivePlayer
+        {
+            get
+            {
+                return this.activePlayer;
+            }
+            set
+            {
+                activePlayer = value;
+            }
+        }
+        public List<Deck> Decks
+        {
+            get
+            {
+                return this.decks;
+            }
+        }
+        public Board BoardGame
+        {
+            get
+            {
+                return this.boardGame;
+            }
+        }
+        public bool EndGame
+        {
+            get
+            {
+                return this.endGame;
+            }
+            set
+            {
+                endGame = value;
+            }
+        }
+
+        //Metodos
+        public bool CheckIfEndGame()
+        {
+            if (players[0].LifePoints == 0 || players[1].LifePoints == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public int GetWinner()
+        {
+            if (players[0].LifePoints == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+        public void Play()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
